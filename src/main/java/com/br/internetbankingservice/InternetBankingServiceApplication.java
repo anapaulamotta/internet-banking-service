405f08164e832c47630fa2db3b23841a05124b1e
anapaulamotta <mottaanapaula98@gmail.com>
2023-06-07 01:57:37 -0300

    :tada: Initial commit

---
+++ src/main/java/com/br/internetbankingservice/InternetBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.internetbankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/internetbankingservice/InternetBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.internetbankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
